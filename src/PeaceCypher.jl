ab36a2f52638d5fd36f85c5607d0e0c0ba0103a9
akels <akels14@gmail.com>
2020-02-29 20:49:34 +0100

    prep for release

---
+++ src/PeaceCypher.jl
@@ -0,0 +1,49 @@
+module PeaceCypher
+
+### This is a place of cryptographic definitions for Notary and Cypher. 
+using PeaceVote: CypherSuite
+import PeaceVote: Notary, Cypher
+
+using Random
+using CryptoGroups
+using CryptoSignatures
+using SecureIO
+using Nettle
+
+
+ThisCypherSuite = CypherSuite(@__MODULE__)
+
+
+function rngint(len::Integer)
+    max_n = ( BigInt(1) << len ) - 1
+    if len > 2
+        min_n = BigInt(1) << (len - 1)
+        return rand(min_n:max_n)
+    end
+    return rand(1:max_n)
+end
+
+
+function Notary(::Type{ThisCypherSuite},config::Symbol)
+
+    G = CryptoGroups.Scep256k1Group()
+    hash(x::AbstractString) = parse(BigInt,Nettle.hexdigest("sha256",x),base=16)
+
+    Signer() = CryptoSignatures.Signer(G)
+    Signature(x::AbstractString,signer) = CryptoSignatures.DSASignature(hash(x),signer)
+    verify(data,signature) = CryptoSignatures.verify(signature,G) && hash(data)==signature.hash ? hash("$(signature.pubkey)") : nothing
+
+    return Notary(Signer,Signature,verify,hash)
+end
+
+function Cypher(::Type{ThisCypherSuite},config::Symbol)
+    G = CryptoGroups.Scep256k1Group()
+    rng() = rngint(100)
+    secureio(socket,key) = SecureSocket(socket,key)
+
+    return Cypher(G,rng,secureio)
+end
+
+export Notary, Cypher
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,22 @@
+using PeaceCypher
+using PeaceVote
+
+uuid = PeaceVote.uuid(:PeaceCypher)
+
+notary = Notary(CypherSuite(uuid),:default)
+
+signer = notary.Signer()
+
+msg = "Hello World"
+signature = notary.Signature(msg,signer)
+@show notary.verify(msg,signature)
+
+cypher = Cypher(CypherSuite(uuid),:default)
+
+@show cypher.G
+@show cypher.rng()
+
+io = IOBuffer()
+secureio = cypher.secureio(io,24235235)
+write(secureio,b"hello")
+take!(io)
